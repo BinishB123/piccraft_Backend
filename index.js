ea3fb3b1c06af49c4b2745d1abeca7bdf62d73a2
BinishB123 <binishb2018@gmail.com>
2025-02-11 19:24:07 +0530

    okkk

--- index.js
+++ index.js
@@ -30,13 +30,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin:"*",
     methods: "GET, PUT, POST, PATCH, OPTIONS, DELETE",
     allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
     credentials: true, // Allow cookies
